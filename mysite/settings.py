d71fe9c78a048be0a6d92f6ec112d129d9a5a0fc
sri-vathsa <vathsa.vemula@codenation.co.in>
2021-04-28 19:48:55 +0530

    Added code

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-20niy^8^q+1=wpytn6oes**yk775*5k22%dhvjb#8ag5hke1%e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
